4386d028b4d058fdac1efda54c1d35dbc865089e
Aldo-Becker <113803505+Aldo-Becker@users.noreply.github.com>
2023-03-23 21:45:49 -0600

    prueba github

---
+++ Ticket/Entidades/Usuario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuario
+    {
+        public string CodigoUsuario { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Contraseña { get; set; }
+
+        public string Rol { get; set; }
+
+        public string Correo { get; set; }
+
+        public byte[] Foto { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public bool EstaActivo { get; set; }
+
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(string codigoUsuario, string nombre, string contraseña, string rol, string correo, byte[] foto, DateTime fechaCreacion, bool estaActivo)
+        {
+            CodigoUsuario = codigoUsuario;
+            Nombre = nombre;
+            Contraseña = contraseña;
+            Rol = rol;
+            Correo = correo;
+            Foto = foto;
+            FechaCreacion = fechaCreacion;
+            EstaActivo = estaActivo;
+        }
+
+    }
+}
---
+++ Ticket/Vista/LoginForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelarButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void AceptarButton_Click(object sender, EventArgs e)
+        {
+            if (UsuarioTextBox.Text == String.Empty)
+            {
+                errorProvider1.SetError(UsuarioTextBox, "Ingrese un Usuario");
+                return;
+            }
+            errorProvider1.Clear();
+            if (string.IsNullOrEmpty(ContraseñaTextBox.Text))
+            {
+                errorProvider1.SetError(ContraseñaTextBox, "Ingrese una Contraseña");
+                ContraseñaTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+
+            //Login login = new Login(UsuarioTextBox.Text, ContraseñaTextBox.Text);
+            //Usuario usuario = new Usuario();
+            //UsuarioDB usuarioDB = new UsuarioDB();
+
+            //usuario = usuarioDB.Auntenticar(login);
+
+            MenuForm menuFormulario = new MenuForm();
+            this.Hide();
+            menuFormulario.Show();
+
+            //if (usuario != null)
+            //{
+            //    if (usuario.EstaActivo)
+            //    {
+            //        System.Security.Principal.GenericIdentity identidad = new System.Security.Principal.GenericIdentity(usuario.CodigoUsuario);
+            //        System.Security.Principal.GenericPrincipal principal = new System.Security.Principal.GenericPrincipal(identidad, new string[] { usuario.Rol });
+            //        System.Threading.Thread.CurrentPrincipal = principal;
+
+            //        //Mostramos el Menú
+            //        Menu menuFormulario = new Menu();
+            //        this.Hide();
+            //        menuFormulario.Show();
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("El Usuario no Esta Activo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    }
+
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Datos de Usuario Incorrectos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+
+            ////instanciar objeto
+
+        }
+
+        private void MostrarContraseñaButton_Click(object sender, EventArgs e)
+        {
+            if (ContraseñaTextBox.PasswordChar == '*')
+            {
+                ContraseñaTextBox.PasswordChar = '\0';
+            }
+            else
+            {
+                ContraseñaTextBox.PasswordChar = '*';
+            }
+        }
+    }
+}
